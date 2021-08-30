a95bcb79d74da6bc0e926bdf89333320015f8e26
Ehab Rafat <73589240+ehabrafat@users.noreply.github.com>
2021-08-30 09:41:02 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-public static void print(List<String> path){
+    public static void print(List<String> paths){
         String[] lastPath = {"/"};
-        for(int i = 0; i< path.size(); i++){
-            String[] branches = path.get(i).split("/");
+        for(String path : paths){
+            String[] branches = path.split("/");
             int l = 0, b = 0;
             while (l < lastPath.length && b < branches.length){
                 if(lastPath[l].equals(branches[b])){
@@ -19,7 +19,7 @@ public static void print(List<String> path){
                     System.out.print(breaker);
                 }
                  else{
-                    System.out.print(" --> " + branches[b]);
+                   break;
                 }
                 ++l;
                 ++b;
